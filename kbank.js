6c464e2128da4ed73e9531e1e7386704d49c32db
djchrisnet <github@djchrisnet.de>
2014-12-29 19:01:35 +0100

    First commit

---
+++ kbank.js
@@ -0,0 +1,152 @@
+/**
+*
+* @package MCA
+* @version $Id$
+* @copyright (c) 2014 djchrisnet
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+var KBank = (new function(data) {
+	var _data = {};
+	
+	/* COMMAND */
+	this.create = function(uid) {
+		if(uid === undefined) {
+			return;
+		}
+		
+		if(_data[uid] === undefined) {
+			_data[uid] = {
+				knuddel: 0.00,
+				buyin: 0.00,
+				payout: 0.00,
+			};
+		}
+	};
+	
+	this.getKn = function(uid) {
+		if(uid === undefined) {
+			return;
+		}
+		
+		this.create(uid);
+		return _data[uid].knuddel;
+	};
+	
+	this.getKonto = function(uid) {
+		if(uid === undefined) {
+			return;
+		}
+		
+		this.create(uid);
+		return _data[uid];
+	};
+
+	this.setKn = function(uid, kn) {
+		if(uid === undefined) {
+			return;
+		}
+		
+		if(kn === undefined) {
+			return;
+		}
+		
+		this.create(uid);
+
+		if(kn < 0) {
+			return false;
+		}
+
+		_data[uid].knuddel = kn;
+	};
+	
+	this.addKn = function(uid, kn) {
+		if(uid === undefined) {
+			return;
+		}
+		
+		if(kn === undefined) {
+			return;
+		}
+		
+		this.create(uid);
+		_data[uid].knuddel += kn;
+	};
+
+	this.rmKn = function(uid, kn) {
+		if(uid === undefined) {
+			return;
+		}
+		
+		if(kn === undefined) {
+			return;
+		}
+		
+		this.create(uid);
+		
+		if(kn < 0) {
+			return false;
+		}
+		
+		if(_data[uid].knuddel < kn) {
+			return false;
+		}
+		
+		_data[uid].knuddel -= kn;
+		return true;
+	};
+	
+	this.payout = function(uid, kn) {
+		if(uid === undefined) {
+			return;
+		}
+		
+		if(kn === undefined) {
+			return;
+		}
+		
+		this.create(uid);
+		_data[uid].knuddel -= kn;
+		_data[uid].payout += kn;		
+	};
+
+	this.payin = function(uid, kn) {
+		if(uid === undefined) {
+			return;
+		}
+		
+		if(kn === undefined) {
+			return;
+		}
+		
+		this.create(uid);
+		_data[uid].knuddel += kn;
+		_data[uid].buyin += kn;
+	};
+	
+	this.getData = function() {
+		return _data;
+	};
+
+	this.setData = function(data) {
+		if(data === undefined) {
+			return;
+		}
+		_data = data;
+	};	
+	
+	this.getUsers = function() {
+		return Object.keys(_data);
+	};
+	
+	this.getTransit = function() {
+		transit = 0.00;
+		
+		for(var uid in _data) {
+			transit += this.getKn(uid);
+		}
+		
+		return transit;
+	};
+}());
